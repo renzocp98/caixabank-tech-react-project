e65783137a2b3b7734d887cae2c4f408c19e1997
renzocp98 <renzo0ncp@gmail.com>
2026-05-07 19:09:55 +0200

    Instalacion de Tailwind

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
 
 export default function HomePage(){
-    const [coindata, setCoindata] = useState(null);
+    const [coindata, setCoindata] = useState([]);
     const API_KEY = import.meta.env.VITE_COINGECKO_API_KEY;
     
     const loadData = async () => {
-        const response = await fetch(`https://api.coingecko.com/api/v3/ping?x_cg_demo_api_key=${API_KEY}`);
+        const response = await fetch(
+            `https://api.coingecko.com/api/v3/global?x_cg_demo_api_key=${API_KEY}`);
         return await response.json();
     }
 
